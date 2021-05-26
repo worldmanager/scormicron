b48f1346f8c2a76bb2d2477e9fd68188070a10c1
Felix Rong <rainmore24@gmail.com>
2021-05-26 10:54:56 +1000

    [WM-13967] Updated scalatest API to the latest version

--- src/test/scala/com/worldmanager/scormicron/BaseSpec.scala
+++ src/test/scala/com/worldmanager/scormicron/BaseSpec.scala
@@ -1,8 +1,11 @@
 package com.worldmanager.scormicron
 
-import org.scalatest.{BeforeAndAfterAll, BeforeAndAfterEach, FunSpec, GivenWhenThen, Matchers}
+import org.scalatest.funspec.AnyFunSpecLike
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.{BeforeAndAfterAll, BeforeAndAfterEach, GivenWhenThen, TestSuite}
 
-abstract class BaseSpec extends FunSpec
+abstract class BaseSpec extends TestSuite
+    with AnyFunSpecLike
     with Matchers
     with GivenWhenThen
     with BeforeAndAfterAll
